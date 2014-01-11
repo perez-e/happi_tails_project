b63b3c2345162a2ec38b0246fae855155f4beab2
Eric Perez <ericricardoperez@gmail.com>
2014-01-10 23:32:16 -0800

    adding clients and animals into the shelter

--- main.rb
+++ main.rb
@@ -33,11 +33,37 @@ while choice != 'q'
     message += "Clients of the #{shelter.name}:\n#{shelter.display_clients.join("\n")}"
     # Display all clients of shelter
   when "3"
-    message += 'Something here'
-    # Display available units
+    # 1.) prompt for name, species, gender, age
+    # 2.) create instance of animal
+    # 3.) place animal in shelter
+    print "What species is the animal? "
+    species = gets.chomp
+    print "What is the name of the #{species}? "
+    name = gets.chomp
+    print "How old is #{name}? "
+    age = gets.to_i
+    print "Is is a male or female? "
+    gender = gets.chomp
+    animal = Animal.new(name, age, gender, species)
+    puts "Added animal to shelter"
+    shelter.add_animal animal
+    puts "\n[Enter to Comtinue]"
+    get
+    message += 'Successfully added #{species} #{name} to #{shelter.name}'
   when "4"
-    message += "Something here"
-    # Display a list of all tenants contact info
+    # 1.) promp for name, age
+    # 2.) create instance of client
+    # 3.) add client to shelter
+    print "What is the client's name? "
+    name = gets.chomp
+    print "How old is #{name}? "
+    age = gets.to_i
+    client  = Client.new(name, age)
+    shelter.add_client client
+    puts "Added client to the shelter"
+    puts "\n[Enter to Continue]"
+    gets
+    message += "Successfully added #{name} as client to #{shelter.name}"
   when "5"
     message += "Something here"
     # Show total sqft rented
