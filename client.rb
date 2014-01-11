d90ecbbb577ccfd7bd5b66247d5fd0ea21eeb104
Eric Perez <ericricardoperez@gmail.com>
2014-01-10 17:36:51 -0800

    added get_info, put_for_adoption, and adopt_animal methods

--- client.rb
+++ client.rb
@@ -1,11 +1,27 @@
 class Client
 	attr_reader :name, :age
 	attr_accessor :children, :pets
-	
+
 	def initialize(name, age)
 		@name = name
 		@age = age
 		@pets = []
 		@children = []
+	end
+
+	def get_info
+		"#{@name}: #{@pets}"
+	end
+
+	def put_for_adoption(name)
+		@pets.each do |pet|
+			if pet.name == name
+				@pets.delete pet
+			end
+		end
+	end
+
+	def adopt_animal(animal)
+		@pets << animal
 	end
 end
