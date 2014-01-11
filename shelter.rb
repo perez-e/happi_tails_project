5592badaeb7b04687138916efb5c858c7f8745fe
Eric Perez <ericricardoperez@gmail.com>
2014-01-10 17:38:48 -0800

    added display_animals, display_clients, and add_animal_to_shelter methods to Shelter class

--- shelter.rb
+++ shelter.rb
@@ -1,6 +1,24 @@
 class Shelter
-	def initialize
+	def initialize(name, address)
+		@name = name
+		@address = address
 		@animals = []
 		@clients = []
+	end
+
+	def display_clients
+		@clients.map do |client|
+			client.get_info
+		end
+	end
+
+	def display_animals
+		@animals.map do |animal|
+			animal.get_info
+		end
+	end
+
+	def add_animal_to_shelter(animal)
+		@animals << animal
 	end
 end
