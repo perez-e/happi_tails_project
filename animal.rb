9669a68a7da805e17510251a98a028ed3b9ec94c
Eric Perez <ericricardoperez@gmail.com>
2014-01-10 23:13:49 -0800

    debugged animal class

--- animal.rb
+++ animal.rb
@@ -1,16 +1,16 @@
-Class Animal
+class Animal
 	attr_accessor :toys
-	attr_reader :name, :age, :gender, :species
+	attr_reader :name, :gender, :species, :age
 	def initialize(name, age, gender, species)
 		@name = name
 		@age = age
 		@gender = gender
 		@species = species
-		@toys = []
+		@toys = toys
 	end
 
 	def get_info
-		"name: #{@name}".ljust(12) + "species: #{@species}".ljust(12) +
-		"age: #{@age}".ljust(12) + "gender: #{@gender}"
+		"name: #{name}".ljust(20)+"species: #{species}".ljust(20)+"age: #{age}".ljust(20) + "gender: #{gender}"
 	end
+	
 end
